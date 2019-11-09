898ff71f0fbf2f9746a144c2a848d373f26a0d21
Richard Zhao <rzhao2@andrew.cmu.edu>
2019-11-08 21:04:37 -0500

    minimalized

--- javascripts/boxes.js
+++ javascripts/boxes.js
@@ -4,20 +4,20 @@
 var button = document.getElementById("archiveButton");
 var boring = document.getElementById("archive");
 var cool = document.getElementById("cool");
-boring.style.display = "none";
-var isBoring = false;
+cool.style.display = "none";
+var isBoring = true;
 
 button.addEventListener("click",function(){
   isBoring = !isBoring;
   
   if(isBoring == true){
-    button.innerHTML = "back";
+    button.innerHTML = "cool";
     boring.style.display = "block";
     cool.style.display = "none";
   }
   else{
     cool.style.display = "block";
-    button.innerHTML = "archive";
+    button.innerHTML = "back";
     boring.style.display = "none";
   }
 },false);
